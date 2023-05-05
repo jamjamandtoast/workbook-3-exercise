b10e7e67d06e8475964d6a6e535cf5530db9c56a
jamjamandtoast <jroblero0@gmail.com>
2023-05-05 01:48:03 -0400

    please commit

--- part_codes.js
+++ part_codes.js
@@ -1,23 +1,45 @@
 "use strict";
-
 function getSupplier(code) {
- let supplier;
+    let supplier = code.split(":")[0];
+    let applesTea = code.indexOf("0,-");
+    let partCode = code.substring(supplier.length + 1, applesTea);
+  
+    // do the work...
+  
+    return supplier;
+  }
 
- //do the work...
-
- supplier = "some value to be replaced later";
-
- return supplier;
+  function getProductNumber(code) {
+    let colonPos = code.indexOf(":");
+    let dashPos = code.indexOf("-");
+    let productNum = code.substring(colonPos + 1, dashPos);
     
-}
+    return productNum;
+  }
+  
+  function getSize(code){
+    let size = code.split("-")[1];
+    let sizeHelp = ("0,-");
+    let sizeCode = code.substring(size.length +1, sizeHelp);
+    return size;
+
+  }
+  
+
+  
+  let code1 = "ACME:123-L the large (L) part 123 is supplied by ACME";
 
 
-let code1 = "ACME:123-L";
-let code2 = "DI:12345-M";
-let code3 = "ACE:1-12";
+  let code1Supplier = getSupplier(code1);
+  
+  console.log(`Code1 (full)    : ${code1}`);
+  console.log(`Code 1 supplier : ${code1Supplier}`);
+  
 
+  
+let code1Products = getProductNumber(code1);
+console.log("product number: " + code1Products);
 
-let code1Supplier = getSupplier(code1);
+ let code1Size = getSize(code1);
+ console.log("product size: " + code1Size);
 
-console.log(`Code1 (full)    : ${code1}`);
-console.log(`Code 1 supplier : ${code1Supplier}`);
